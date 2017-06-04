8a574c7584efa4f65c3ebd6989894a2df91078cd
aegisenergy <feng.j@aegisenergy.co>
2017-06-04 09:37:51 -0700

    init

---
+++ Assets/controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+
+    private bool walk = false;
+    private Vector3 spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        spawnPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (walk) transform.position += Camera.main.transform.forward * 0.5f * Time.deltaTime;
+
+        if (transform.position.y < -10f)
+        {
+            transform.position = spawnPoint;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(.5f, .5f, 0));
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name.Contains("Plane"))
+            {
+                walk = false;
+            }
+            else
+            {
+                walk = true;
+            }
+        }
+	}
+}
